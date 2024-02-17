5483e644036b2d67de36ca64007dc87bdd6d11ef
Tamer Eldeeb <tamer.eldeeb@columbia.edu>
2024-02-17 11:52:37 -0500

    minor schema fixes

--- rangeserver/src/persistence/cassandra.rs
+++ rangeserver/src/persistence/cassandra.rs
@@ -1,6 +1,20 @@
 use super::*;
+use scylla::{Session, SessionBuilder};
 
-pub struct Cassandra {}
+pub struct Cassandra {
+    session: Session,
+}
+
+impl Cassandra {
+    async fn create_test() -> Cassandra {
+        let session = SessionBuilder::new()
+            .known_node("127.0.0.1:9042".to_string())
+            .build()
+            .await
+            .unwrap();
+        Cassandra { session }
+    }
+}
 
 impl Persistence for Cassandra {
     async fn take_ownership_and_load_range(&self, _range_id: Uuid) -> Result<RangeInfo, Error> {
