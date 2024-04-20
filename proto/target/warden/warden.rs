22673070121a9094a46b7bb5fde6087922931bf1
Tamer Eldeeb <teeldeeb@gmail.com>
2024-04-20 09:02:09 -0400

    Initial Warden Service protocol (#24)
    proto and rpc definition of warden service.
    
    The warden is a regional service responsible for load balancing and assigning key ranges to range servers.
    
    Range servers register themselves with the regional warden, and receive assignment updates.

--- proto/target/warden/warden.rs
+++ proto/target/warden/warden.rs
@@ -1,6 +1,168 @@
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct HelloRequest {
+pub struct RangeId {
     #[prost(string, tag = "1")]
-    pub name: ::prost::alloc::string::String,
+    pub keyspace_id: ::prost::alloc::string::String,
+    #[prost(string, tag = "2")]
+    pub range_id: ::prost::alloc::string::String,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct HostInfo {
+    #[prost(string, tag = "1")]
+    pub identity: ::prost::alloc::string::String,
+    #[prost(string, tag = "2")]
+    pub zone: ::prost::alloc::string::String,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct FullAssignment {
+    #[prost(message, repeated, tag = "1")]
+    pub range: ::prost::alloc::vec::Vec<RangeId>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct IncrementalAssignment {
+    #[prost(message, repeated, tag = "1")]
+    pub load: ::prost::alloc::vec::Vec<RangeId>,
+    #[prost(message, repeated, tag = "2")]
+    pub unload: ::prost::alloc::vec::Vec<RangeId>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct WardenUpdate {
+    #[prost(oneof = "warden_update::Update", tags = "1, 2")]
+    pub update: ::core::option::Option<warden_update::Update>,
+}
+/// Nested message and enum types in `WardenUpdate`.
+pub mod warden_update {
+    #[allow(clippy::derive_partial_eq_without_eq)]
+    #[derive(Clone, PartialEq, ::prost::Oneof)]
+    pub enum Update {
+        #[prost(message, tag = "1")]
+        FullAssignment(super::FullAssignment),
+        #[prost(message, tag = "2")]
+        IncrementalAssignment(super::IncrementalAssignment),
+    }
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct RegisterRangeServerRequest {
+    #[prost(message, optional, tag = "1")]
+    pub range_server: ::core::option::Option<HostInfo>,
+}
+/// Generated client implementations.
+pub mod warden_client {
+    #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
+    use tonic::codegen::*;
+    use tonic::codegen::http::Uri;
+    #[derive(Debug, Clone)]
+    pub struct WardenClient<T> {
+        inner: tonic::client::Grpc<T>,
+    }
+    impl WardenClient<tonic::transport::Channel> {
+        /// Attempt to create a new client by connecting to a given endpoint.
+        pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
+        where
+            D: TryInto<tonic::transport::Endpoint>,
+            D::Error: Into<StdError>,
+        {
+            let conn = tonic::transport::Endpoint::new(dst)?.connect().await?;
+            Ok(Self::new(conn))
+        }
+    }
+    impl<T> WardenClient<T>
+    where
+        T: tonic::client::GrpcService<tonic::body::BoxBody>,
+        T::Error: Into<StdError>,
+        T::ResponseBody: Body<Data = Bytes> + Send + 'static,
+        <T::ResponseBody as Body>::Error: Into<StdError> + Send,
+    {
+        pub fn new(inner: T) -> Self {
+            let inner = tonic::client::Grpc::new(inner);
+            Self { inner }
+        }
+        pub fn with_origin(inner: T, origin: Uri) -> Self {
+            let inner = tonic::client::Grpc::with_origin(inner, origin);
+            Self { inner }
+        }
+        pub fn with_interceptor<F>(
+            inner: T,
+            interceptor: F,
+        ) -> WardenClient<InterceptedService<T, F>>
+        where
+            F: tonic::service::Interceptor,
+            T::ResponseBody: Default,
+            T: tonic::codegen::Service<
+                http::Request<tonic::body::BoxBody>,
+                Response = http::Response<
+                    <T as tonic::client::GrpcService<tonic::body::BoxBody>>::ResponseBody,
+                >,
+            >,
+            <T as tonic::codegen::Service<
+                http::Request<tonic::body::BoxBody>,
+            >>::Error: Into<StdError> + Send + Sync,
+        {
+            WardenClient::new(InterceptedService::new(inner, interceptor))
+        }
+        /// Compress requests with the given encoding.
+        ///
+        /// This requires the server to support it otherwise it might respond with an
+        /// error.
+        #[must_use]
+        pub fn send_compressed(mut self, encoding: CompressionEncoding) -> Self {
+            self.inner = self.inner.send_compressed(encoding);
+            self
+        }
+        /// Enable decompressing responses.
+        #[must_use]
+        pub fn accept_compressed(mut self, encoding: CompressionEncoding) -> Self {
+            self.inner = self.inner.accept_compressed(encoding);
+            self
+        }
+        /// Limits the maximum size of a decoded message.
+        ///
+        /// Default: `4MB`
+        #[must_use]
+        pub fn max_decoding_message_size(mut self, limit: usize) -> Self {
+            self.inner = self.inner.max_decoding_message_size(limit);
+            self
+        }
+        /// Limits the maximum size of an encoded message.
+        ///
+        /// Default: `usize::MAX`
+        #[must_use]
+        pub fn max_encoding_message_size(mut self, limit: usize) -> Self {
+            self.inner = self.inner.max_encoding_message_size(limit);
+            self
+        }
+        /// Called by a range server to register itself with its regional warden.
+        /// Establishes a long-lived streamfor the range server to continuously receive
+        /// assignment updates from the warden.
+        pub async fn register_range_server(
+            &mut self,
+            request: impl tonic::IntoRequest<super::RegisterRangeServerRequest>,
+        ) -> std::result::Result<
+            tonic::Response<tonic::codec::Streaming<super::WardenUpdate>>,
+            tonic::Status,
+        > {
+            self.inner
+                .ready()
+                .await
+                .map_err(|e| {
+                    tonic::Status::new(
+                        tonic::Code::Unknown,
+                        format!("Service was not ready: {}", e.into()),
+                    )
+                })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static(
+                "/warden.Warden/RegisterRangeServer",
+            );
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(GrpcMethod::new("warden.Warden", "RegisterRangeServer"));
+            self.inner.server_streaming(req, path, codec).await
+        }
+    }
 }
