65241fe50e6187c1821684379f61f9f5e428e310
Tamer Eldeeb <teeldeeb@gmail.com>
2024-05-20 21:52:20 -0400

    FastNetwork Trait (#36)
    A trait to be used for kernel-bypass poll-based networking.
    
    This is intended as a wrapper over machnet, with an additional udp socket implementation used in testing.

---
+++ common/src/network/fast_network.rs
@@ -0,0 +1,14 @@
+use std::net::SocketAddr;
+
+use bytes::Bytes;
+use tokio::sync::mpsc;
+// Busy-polled network endpoint.
+pub trait FastNetwork: Send + Sync + 'static {
+    fn send(&self, to: SocketAddr, payload: Bytes);
+    // Listen for messages sent from a specific SocketAddr.
+    fn register(&self, from: SocketAddr) -> mpsc::UnboundedReceiver<Bytes>;
+    // Reads one message from the network (if any) and delivers it to all the
+    // relevant listeners. Returns true if something was read from the network,
+    // and false otherwise.
+    fn poll(&self) -> bool;
+}
