8585fd9436b548f46ab2e8a85475c3be138b692b
Tamer Eldeeb <tamer.eldeeb@columbia.edu>
2024-07-13 15:17:31 -0400

    epoch broadcaster

--- flatbuf/src/lib.rs
+++ flatbuf/src/lib.rs
@@ -1,3 +1,7 @@
 #[allow(non_snake_case)]
+#[path = "../target/epoch_broadcaster/schema_generated.rs"]
+pub mod epoch_broadcaster_flatbuffers;
+
+#[allow(non_snake_case)]
 #[path = "../target/rangeserver/schema_generated.rs"]
 pub mod rangeserver_flatbuffers;
