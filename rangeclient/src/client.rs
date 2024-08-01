5459eb27dda72c94fde6e5a8f4a8728e37395dbe
tgraham89 <thomashgraham@gmail.com>
2024-07-31 18:23:26 -0700

    basic client prefetch function

--- rangeclient/src/client.rs
+++ rangeclient/src/client.rs
@@ -8,12 +8,15 @@ use flatbuf::rangeserver_flatbuffers::range_server::Record as FlatbufRecord;
 use flatbuf::rangeserver_flatbuffers::range_server::TransactionInfo as FlatbufTransactionInfo;
 use flatbuf::rangeserver_flatbuffers::range_server::*;
 use flatbuffers::FlatBufferBuilder;
+use proto::rangeserver::range_server_client::RangeServerClient;
+use proto::rangeserver::{PrefetchRequest, RangeId, RangeKey};
 use rangeserver::error::Error as RangeServerError;
 use rangeserver::transaction_info::TransactionInfo;
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::{oneshot, Mutex};
 use tokio_util::sync::CancellationToken;
+use tonic::Request;
 use uuid::Uuid;
 
 pub struct PrepareOk {
@@ -415,5 +418,48 @@ impl RangeClient {
         );
         fbb.finish(fbb_root, None);
         fbb.finished_data()
+    }
+
+    pub async fn prefetch(
+        &self,
+        tx: Arc<TransactionInfo>,
+        range_id: &FullRangeId,
+        keys: Vec<Bytes>,
+    ) -> Result<(), RangeServerError> {
+        // Connect to the gRPC server
+        let mut client = RangeServerClient::connect("http://[::1]:50051")
+            .await
+            .unwrap();
+
+        // Create a PrefetchRequest
+        let transaction_id = tx.id.to_string(); // Generate a new UUID for the transaction_id
+                                                // let key = Bytes::from("your_key".to_string()); // Replace "your_key" with the actual key value
+        let keyspace_id = range_id.keyspace_id.id.to_string();
+        let range_id = range_id.range_id.to_string();
+
+        let range = RangeId {
+            keyspace_id,
+            range_id,
+        };
+
+        let range_keys: Vec<RangeKey> = keys
+            .into_iter()
+            .map(|key| RangeKey {
+                range: Some(range.clone()),
+                key: key.to_vec(),
+            })
+            .collect();
+
+        let request = PrefetchRequest {
+            transaction_id,
+            range_key: range_keys,
+        };
+
+        // Send the request
+        let response = client.prefetch(Request::new(request)).await.unwrap();
+
+        println!("RESPONSE={:?}", response);
+
+        Ok(())
     }
 }
