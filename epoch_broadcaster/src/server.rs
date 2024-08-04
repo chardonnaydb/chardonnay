161d64d7374d6ecd834e15c9693b7cb0e18d7c8d
Tamer Eldeeb <tamer.eldeeb@columbia.edu>
2024-08-04 12:32:58 -0400

    Epoch Broadcaster frontend

--- epoch_broadcaster/src/server.rs
+++ epoch_broadcaster/src/server.rs
@@ -1,14 +1,22 @@
+use flatbuffers::FlatBufferBuilder;
 use std::net::SocketAddr;
 use std::str::FromStr;
 use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering::SeqCst;
 use std::sync::Arc;
 
+use bytes::Bytes;
+use common::network::fast_network::FastNetwork;
 use proto::epoch_broadcaster::epoch_broadcaster_server::{
     EpochBroadcaster, EpochBroadcasterServer,
 };
 use proto::epoch_broadcaster::{SetEpochRequest, SetEpochResponse};
+use tokio_util::sync::CancellationToken;
 use tonic::{transport::Server as TServer, Request, Response, Status as TStatus};
+
+use flatbuf::epoch_broadcaster_flatbuffers::epoch_broadcaster::*;
+
+type DynamicErr = Box<dyn std::error::Error + Sync + Send + 'static>;
 
 pub struct Server {
     epoch: AtomicUsize,
@@ -39,26 +47,156 @@ impl EpochBroadcaster for ProtoServer {
     }
 }
 
-pub fn new(bg_runtime: tokio::runtime::Handle) -> Arc<Server> {
-    Arc::new(Server {
-        epoch: AtomicUsize::new(0),
-        bg_runtime,
-    })
-}
+impl Server {
+    async fn read_epoch(
+        &self,
+        network: Arc<dyn FastNetwork>,
+        sender: SocketAddr,
+        request: ReadEpochRequest<'_>,
+    ) -> Result<(), DynamicErr> {
+        let mut fbb = FlatBufferBuilder::new();
+        let fbb_root = match request.request_id() {
+            None => ReadEpochResponse::create(
+                &mut fbb,
+                &ReadEpochResponseArgs {
+                    request_id: None,
+                    status: Status::InvalidRequestFormat,
+                    epoch: 0,
+                },
+            ),
+            Some(request_id) => {
+                let epoch = self.epoch.load(SeqCst);
+                let status = if epoch <= 0 {
+                    Status::EpochUnknown
+                } else {
+                    Status::Ok
+                };
+                let request_id = Some(Uuidu128::create(
+                    &mut fbb,
+                    &Uuidu128Args {
+                        lower: request_id.lower(),
+                        upper: request_id.upper(),
+                    },
+                ));
+                ReadEpochResponse::create(
+                    &mut fbb,
+                    &ReadEpochResponseArgs {
+                        request_id,
+                        status,
+                        epoch: epoch as u64,
+                    },
+                )
+            }
+        };
 
-pub fn start(server: Arc<Server>) {
-    let proto_server = ProtoServer {
-        server: server.clone(),
-    };
-    server.bg_runtime.spawn(async move {
-        // TODO(tamer): make this configurable.
-        let addr = SocketAddr::from_str("127.0.0.1:10010").unwrap();
-        if let Err(e) = TServer::builder()
-            .add_service(EpochBroadcasterServer::new(proto_server))
-            .serve(addr)
-            .await
-        {
-            println!("Unable to start proto server: {}", e);
+        fbb.finish(fbb_root, None);
+        self.send_response(network, sender, MessageType::ReadEpoch, fbb.finished_data())?;
+        Ok(())
+    }
+
+    async fn handle_message(
+        server: Arc<Self>,
+        fast_network: Arc<dyn FastNetwork>,
+        sender: SocketAddr,
+        msg: Bytes,
+    ) -> Result<(), DynamicErr> {
+        // TODO: gracefully handle malformed messages instead of unwrapping and crashing.
+        let msg = msg.to_vec();
+        let envelope = flatbuffers::root::<RequestEnvelope>(msg.as_slice())?;
+        match envelope.type_() {
+            MessageType::ReadEpoch => {
+                let req = flatbuffers::root::<ReadEpochRequest>(envelope.bytes().unwrap().bytes())?;
+                server.read_epoch(fast_network.clone(), sender, req).await?
+            }
+            _ => (), // TODO: return and log unknown message type error.
+        };
+        Ok(())
+    }
+
+    fn send_response(
+        &self,
+        fast_network: Arc<dyn FastNetwork>,
+        sender: SocketAddr,
+        msg_type: MessageType,
+        msg_payload: &[u8],
+    ) -> Result<(), std::io::Error> {
+        // TODO: many allocations and copies in this function.
+        let mut fbb = FlatBufferBuilder::new();
+        let bytes = fbb.create_vector(msg_payload);
+        let fb_root = ResponseEnvelope::create(
+            &mut fbb,
+            &ResponseEnvelopeArgs {
+                type_: msg_type,
+                bytes: Some(bytes),
+            },
+        );
+        fbb.finish(fb_root, None);
+        let response = Bytes::copy_from_slice(fbb.finished_data());
+        fast_network.send(sender, response)
+    }
+
+    async fn network_server_loop(
+        server: Arc<Self>,
+        fast_network: Arc<dyn FastNetwork>,
+        cancellation_token: CancellationToken,
+    ) {
+        let mut network_receiver = fast_network.listen_default();
+        loop {
+            let () = tokio::select! {
+                () = cancellation_token.cancelled() => {
+                    return ()
+                }
+                maybe_message = network_receiver.recv() => {
+                    match maybe_message {
+                        None => {
+                            println!("fast network closed unexpectedly!");
+                            cancellation_token.cancel()
+                        }
+                        Some((sender, msg)) => {
+                            let server = server.clone();
+                            let fast_network = fast_network.clone();
+                            tokio::spawn(async move{
+                                let _ = Self::handle_message(server, fast_network, sender, msg).await;
+                                // TODO log any error here.
+                            });
+
+                        }
+                    }
+                }
+            };
         }
-    });
+    }
+
+    pub fn new(bg_runtime: tokio::runtime::Handle) -> Arc<Server> {
+        Arc::new(Server {
+            epoch: AtomicUsize::new(0),
+            bg_runtime,
+        })
+    }
+
+    pub fn start(
+        server: Arc<Server>,
+        fast_network: Arc<dyn FastNetwork>,
+        cancellation_token: CancellationToken,
+    ) {
+        let proto_server = ProtoServer {
+            server: server.clone(),
+        };
+        server.bg_runtime.spawn(async move {
+            // TODO(tamer): make this configurable.
+            let addr = SocketAddr::from_str("127.0.0.1:10010").unwrap();
+            if let Err(e) = TServer::builder()
+                .add_service(EpochBroadcasterServer::new(proto_server))
+                .serve(addr)
+                .await
+            {
+                println!("Unable to start proto server: {}", e);
+            }
+        });
+
+        tokio::spawn(async move {
+            let _ = Self::network_server_loop(server, fast_network, cancellation_token).await;
+            println!("Network server loop exited!")
+        });
+    }
 }
--- rangeserver/src/server.rs
+++ rangeserver/src/server.rs
@@ -18,8 +18,8 @@ use uuid::Uuid;
 use crate::transaction_info::TransactionInfo;
 use crate::warden_handler::WardenHandler;
 use crate::{
-    epoch_provider::EpochProvider, error::Error, for_testing::in_memory_wal::InMemoryWal,
-    range_manager::RangeManager, storage::Storage, cache::Cache, cache::memtabledb::MemTableDB, cache::CacheOptions,
+    cache::Cache, cache::CacheOptions, epoch_provider::EpochProvider, error::Error,
+    for_testing::in_memory_wal::InMemoryWal, range_manager::RangeManager, storage::Storage,
 };
 use flatbuf::rangeserver_flatbuffers::range_server::TransactionInfo as FlatbufTransactionInfo;
 use flatbuf::rangeserver_flatbuffers::range_server::*;
@@ -735,6 +735,7 @@ where
 
 #[cfg(test)]
 pub mod tests {
+    use crate::cache::memtabledb::MemTableDB;
     use common::config::{RangeServerConfig, RegionConfig};
     use common::network::for_testing::udp_fast_network::UdpFastNetwork;
     use common::region::{Region, Zone};
