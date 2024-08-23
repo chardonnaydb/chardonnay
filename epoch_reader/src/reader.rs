37cd255c90e4fc5ee35b607eb2db5c9e12ac1245
Tamer Eldeeb <tamer.eldeeb@columbia.edu>
2024-08-23 18:33:38 -0400

    Use JoinSet in EpochReader

--- epoch_reader/src/reader.rs
+++ epoch_reader/src/reader.rs
@@ -3,6 +3,7 @@ use std::{collections::HashMap, sync::Arc};
 use common::{config::EpochPublisherSet, host_info::HostInfo, network::fast_network::FastNetwork};
 use epoch_publisher::{client::EpochPublisherClient, error::Error};
 use tokio::sync::mpsc;
+use tokio::task::JoinSet;
 use tokio_util::sync::CancellationToken;
 
 /// EpochReader reads the latest epoch from an EpochPublisherSet.
@@ -46,25 +47,19 @@ impl EpochReader {
     }
 
     pub async fn read_epoch(&self) -> Result<u64, Error> {
-        let num_publishers = self.clients.len();
-        let (rx, mut tx) = mpsc::channel(num_publishers);
         // Fire off a request to read the epoch from each publisher in the set in parallel.
+        let mut join_set = JoinSet::new();
         for c in &self.clients {
             let client = c.clone();
-            let rx = rx.clone();
-            self.runtime.spawn(async move {
-                let res = client.read_epoch().await;
-                rx.send(res).await.unwrap();
-            });
+            join_set.spawn_on(async move { client.read_epoch().await }, &self.runtime);
         }
 
         // Now see if any value is returned by a majority of publishers, and return it.
         let mut epoch_value_counts = HashMap::<u64, u64>::new();
-        for _ in 0..num_publishers {
-            let res = tx.recv().await;
+        while let Some(res) = join_set.join_next().await {
             let res = match res {
-                None => return Err(Error::EpochUnknown),
-                Some(res) => res,
+                Err(_) => continue, // TODO: maybe log the error here
+                Ok(res) => res,
             };
             let epoch = match res {
                 Err(_) => continue, // TODO: maybe log the error here
