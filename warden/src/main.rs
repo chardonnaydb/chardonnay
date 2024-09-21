f25f5b70bd275b72e4588fc22d593edcd1e34cc8
Tamer Eldeeb <tamer.eldeeb@columbia.edu>
2024-09-21 19:54:29 -0400

    unmock warden

--- warden/src/main.rs
+++ warden/src/main.rs
@@ -1,3 +1,5 @@
+use common::config::Config;
+use common::region::{Region, Zone};
 use server::run_warden_server;
 use tokio_util::sync::CancellationToken;
 use tracing::info;
@@ -11,9 +13,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let subscriber = tracing_subscriber::fmt::Subscriber::new();
     tracing::subscriber::set_global_default(subscriber)?;
     info!("Hello, Warden!");
-    // TODO(purujit): set up map computation and plug it in.
-    let addr = "[::1]:50051";
+    // TODO(tamer): take the config path as an argument.
+    let config: Config =
+        serde_json::from_str(&std::fs::read_to_string("config.json").unwrap()).unwrap();
+    //TODO(tamer): the region should be passed in as an argument or environment.
+    let region = Region {
+        cloud: None,
+        name: "test-region".into(),
+    };
+    let region_config = config.regions.get(&region).unwrap();
+    let addr = region_config.warden_address.to_string();
     let token = CancellationToken::new();
+    // TODO(purujit): set up map computation and plug it in.
     run_warden_server(addr, tokio::runtime::Handle::current(), token).await?;
     Ok(())
 }
