85d5b273fe505e2d7f40066bcc54d3ae53119251
Tamer Eldeeb <teeldeeb@gmail.com>
2024-09-21 21:11:11 -0400

    Unmock components in rangeserver binary (#88)

--- rangeserver/src/main.rs
+++ rangeserver/src/main.rs
@@ -7,10 +7,7 @@ use common::{
     network::{fast_network::FastNetwork, for_testing::udp_fast_network::UdpFastNetwork},
     region::{Region, Zone},
 };
-use rangeserver::{
-    cache::memtabledb::MemTableDB, for_testing::mock_warden::MockWarden, server::Server,
-    storage::cassandra::Cassandra,
-};
+use rangeserver::{cache::memtabledb::MemTableDB, server::Server, storage::cassandra::Cassandra};
 use tokio::runtime::Builder;
 use tokio_util::sync::CancellationToken;
 
@@ -20,6 +17,7 @@ fn main() {
     let config: Config =
         serde_json::from_str(&std::fs::read_to_string("config.json").unwrap()).unwrap();
     let runtime = Builder::new_current_thread().enable_all().build().unwrap();
+    let runtime_handle = runtime.handle().clone();
     let fast_network = Arc::new(UdpFastNetwork::new(
         UdpSocket::bind(format!(
             "127.0.0.1:{}",
@@ -35,20 +33,25 @@ fn main() {
         }
     });
     let server_handle = runtime.spawn(async move {
+        let cancellation_token = CancellationToken::new();
         let host_info = get_host_info();
-        let mock_warden = MockWarden::new();
-        let warden_address = config
-            .regions
-            .get(&host_info.zone.region)
-            .unwrap()
-            .warden_address;
-        let _ = mock_warden.start(Some(warden_address)).await.unwrap();
+        let region_config = config.regions.get(&host_info.zone.region).unwrap();
+        let publisher_set = region_config
+            .epoch_publishers
+            .iter()
+            .find(|&s| s.zone == host_info.zone)
+            .unwrap();
         let storage = Arc::new(Cassandra::new("127.0.0.1:9042".to_string()).await);
-        let epoch_supplier =
-            Arc::new(rangeserver::for_testing::epoch_supplier::EpochSupplier::new());
-
         // TODO: set number of threads and pin to cores.
         let bg_runtime = Builder::new_multi_thread().enable_all().build().unwrap();
+
+        let epoch_supplier = Arc::new(rangeserver::epoch_supplier::reader::Reader::new(
+            fast_network.clone(),
+            runtime_handle,
+            bg_runtime.handle().clone(),
+            publisher_set.clone(),
+            cancellation_token.clone(),
+        ));
         let server = Server::<_, MemTableDB>::new(
             config,
             host_info,
@@ -56,7 +59,7 @@ fn main() {
             epoch_supplier,
             bg_runtime.handle().clone(),
         );
-        let res = Server::start(server, fast_network, CancellationToken::new())
+        let res = Server::start(server, fast_network, cancellation_token)
             .await
             .unwrap();
         res.await.unwrap()
